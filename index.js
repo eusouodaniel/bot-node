06812f64d1e2bd8d819fd07553c131ecaa505ed5
Daniel Rodrigues <danielrodriguesdrs331@gmail.com>
2019-09-22 15:55:41 -0300

    new(): Integrate with dialogflow

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ var bodyParser = require('body-parser');
 var request = require('request');
 const server = express();
 
+const projectId = 'translate-lusmcc';
+const sessionId = '123456';
+const languageCode = 'pt-BR';
+
+const dialogflow = require('dialogflow');
+
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(express.json());
 server.listen(3000);
+
+const key = process.env.DIALOGFLOW_PRIVATE_KEY.replace(new RegExp("\\\\n", "\g"), "\n")
+
+const config = {
+  credentials: {
+    private_key: key,
+    client_email: process.env.DIALOGFLOW_CLIENT_EMAIL
+  }
+};
+
+const sessionClient = new dialogflow.SessionsClient(config);
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
 server.get('/webhook', (req, res) => {
 	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] == process.env.VERIFY_TOKEN) {
@@ -23,7 +41,8 @@ server.post('/webhook', (req, res) => {
 		data.entry.forEach( function (entry) {
 			entry.messaging.forEach( function (event) {
 				if (event.message) {
-					treatMessage(event);
+				    dialogFlow(event);
+				// 	treatMessage(event);
 				} 
 			})
 		})
@@ -33,27 +52,55 @@ server.post('/webhook', (req, res) => {
 });
 
 
-function treatMessage(event) {
-	let senderID = event.sender.id;
-	let messageText = event.message.text;
+// function treatMessage(event) {
+// 	let senderID = event.sender.id;
+// 	let messageText = event.message.text;
 
-	if (messageText) {
-		switch (messageText) {
-			case 'Oi':
-				sendMessage(senderID, "Oi, eu sou um robô! :)");
-				break;
+// 	if (messageText) {
+// 		switch (messageText) {
+// 			case 'Oi':
+// 				sendMessage(senderID, "Oi, eu sou um robô! :)");
+// 				break;
 
-			case 'Tchau':
-				sendMessage(senderID, "Que pena que você já vai :(");
-				break;
+// 			case 'Tchau':
+// 				sendMessage(senderID, "Que pena que você já vai :(");
+// 				break;
 
-			default:
-				sendMessage(senderID, "Oi, ainda estou em desenvolvimento, portanto só entendo 'Oi' e 'Tchau' :')");
-		}
-	} 
+// 			default:
+// 				sendMessage(senderID, "Oi, ainda estou em desenvolvimento, portanto só entendo 'Oi' e 'Tchau' :')");
+// 		}
+// 	} 
+// }
+
+function dialogFlow(event) {
+      const userId = event.sender.id;
+      const message = event.message.text;
+
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: message,
+            languageCode: languageCode,
+          },
+        },
+      };
+      
+      sessionClient
+        .detectIntent(request)
+        .then(responses => {
+          console.log(responses);
+          const result = responses[0].queryResult;
+          return sendMessage(userId, result.fulfillmentText);
+        })
+        .catch(err => {
+          console.error('ERROR:', err);
+        });
 }
 
+
 function sendMessage(recipientID, messageText) {
+    console.log(messageText);
 	let messageData = {
 		recipient: {
 			id: recipientID
